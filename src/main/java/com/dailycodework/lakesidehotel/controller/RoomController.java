0f4325130642aeab482af16c2227d30967a61dca
Yuanhong Shi <shiyuanhong@hotmail.com>
2024-02-01 16:26:11 +0800

    first hour work

---
+++ src/main/java/com/dailycodework/lakesidehotel/controller/RoomController.java
@@ -0,0 +1,31 @@
+package com.dailycodework.lakesidehotel.controller;
+
+import com.dailycodework.lakesidehotel.model.Room;
+import com.dailycodework.lakesidehotel.response.RoomResponse;
+import com.dailycodework.lakesidehotel.service.IRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/rooms")
+public class RoomController {
+    private IRoomService roomService;
+
+    @PostMapping("/add/new-room")
+    public ResponseEntity<RoomResponse> addNewRoom(
+            @RequestParam("photo") MultipartFile photo,
+            @RequestParam("roomType") String roomType,
+            @RequestParam("roomPrice") BigDecimal roomPrice) throws SQLException, IOException {
+        Room savedRoom = roomService.addNewRoom(photo, roomType, roomPrice);
+        RoomResponse roomResponse = new RoomResponse(savedRoom.getId(), savedRoom.getRoomType(), savedRoom.getRoomPrice());
+
+        return ResponseEntity.ok(roomResponse);
+    }
+}
---
+++ src/main/java/com/dailycodework/lakesidehotel/repository/RoomRepository.java
@@ -0,0 +1,7 @@
+package com.dailycodework.lakesidehotel.repository;
+
+import com.dailycodework.lakesidehotel.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
